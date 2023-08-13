e2906471cb1aaa78de0539eed3ff1a1ccfc46264
Devika <108735244+Devika72@users.noreply.github.com>
2023-08-13 10:09:12 +0530

    Add files via upload

---
+++ practice/src/App.js
@@ -0,0 +1,76 @@
+import "./App.css";
+import { Exercise as Exercise1 } from "./CharacterCount";
+import { Exercise as Exercise2 } from "./ResetPasswordForm";
+import { Exercise as Exercise3 } from "./ResetPassword";
+import { Exercise as Exercise4 } from "./DisableSubmitButton";
+import { Exercise as Exercise5 } from "./ShowPassword";
+import { Exercise as Exercise6 } from "./FontSizeFamily";
+import { Exercise as Exercise7 } from "./AddToCart";
+import { Exercise as Exercise8 } from "./SwitchTabs";
+import { Exercise as Exercise9 } from "./WarningErrorSuccess";
+import { Exercise as Exercise10 } from "./TodoList";
+import { Exercise as Exercise11 } from "./LightDarkMode";
+import { Exercise as Exercise12 } from "./LikedList";
+import { Exercise as Exercise13 } from "./OutOfStock";
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Exercise 1 :</h1>
+      <Exercise1 />
+      <hr />
+      
+      <h1>Exercise 2 :</h1>
+      <Exercise2 />
+      <hr />
+
+      <h1>Exercise 3 :</h1>
+      <Exercise3 />
+      <hr />
+
+      <h1>Exercise 4 :</h1>
+      <Exercise4/>
+      <hr/>
+
+      <h1>Exercise 5 :</h1>
+      <Exercise5/>
+      <hr/>
+
+      <h1>Exercise 6 :</h1>
+      <Exercise6/>
+      <hr/>
+
+      <h1>Exercise 7 :</h1>
+      <Exercise7/>
+      <hr/>
+
+      <h1>Exercise 8 :</h1>
+      <Exercise8/>
+      <hr/>
+
+      <h1>Exercise 9 :</h1>
+      <Exercise9/>
+      <hr/>
+
+      <h1>Exercise 10 :</h1>
+      <Exercise10/>
+      <hr/>
+
+      <h1>Exercise 11 :</h1>
+      <Exercise11/>
+      <hr/>
+
+      <h1>Exercise 12 :</h1>
+      <Exercise12/>
+      <hr/>
+
+      <h1>Exercise 13 :</h1>
+      <Exercise13/>
+      <hr/>
+ 
+ 
+
+    </div>
+  );
+}
